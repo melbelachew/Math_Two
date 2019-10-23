37787a4b81f2a8ecdc196a57fedbb98324ef6597
Mel <melbelachew@gmail.com>
2019-10-23 12:01:16 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        float mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println(mynumber + " & " + myothernumber);
+    }
+}
